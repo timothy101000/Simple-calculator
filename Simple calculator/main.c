ef1ef69e33ab012ee7fb91131ba5d4d384d58a78
timothy101000 <97958832+timothy101000@users.noreply.github.com>
2022-02-17 22:13:06 +0300

    Add files via upload

---
+++ Simple calculator/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int num1, mun2;
+    int sum, sub, mult, mod;
+    float div;
+
+    /*
+     * prints the result of all arithmetic operations
+     */
+     printf("The sum of the given numbers : %d\n", sum);
+     printf("The difference of the given numbers : %d\n", sub);
+     printf("The product of the given numbers : %d\n", mult);
+     printf("The quotient of the given numbers : %f\n", div);
+     printf("MODULUS = %d\n", mod);
+
+     return 0;
+}
